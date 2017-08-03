6c802cf7c385bf382d0b7a24337e21002b0ab359
evandromadeira <evandromfmadeira@gmail.com>
2017-08-03 16:12:43 -0300

    Versão 3 - Variável 'diasDoMesAnt' movida para a estrura if, para ser criada somente se for usada.

--- datas.js
+++ datas.js
@@ -1,9 +1,9 @@
 //Linguagem JavaScript - Autor: Evandro Madeira
 //Algoritmo para retornar a diferença entre a data atual e a data de nascimento de uma pessoa, em anos, meses e dias.
 
-var dataNasc = new Date("1998-09-02T00:00:00");
+var dataNasc = new Date("1989-12-15T00:00:00"); // Formato Ano / Mês / Dia
 var dataAtual = new Date(); // Maior que data de nascimento
-//var dataAtual = new Date("1991-05-14T00:00:00"); //Linha para testes
+//var dataAtual = new Date("2018-01-14T00:00:00"); //Linha para testes
 
 // 'en-US' >>> Formato americano Mês / Dia / Ano
 console.log("Data Nasc.: " + dataNasc.toLocaleDateString('en-US'));
@@ -13,9 +13,6 @@ var ano = dataAtual.getFullYear() - dataNasc.getFullYear();
 var mes = (dataAtual.getMonth() + 1) - (dataNasc.getMonth() + 1);
 var dia = dataAtual.getDate() - dataNasc.getDate();
 
-var diasDoMesAnt = new Date(Date.parse(new Date(dataAtual.getFullYear(), dataAtual.getMonth(), 1)) - 86400000).getDate();
-// Retorna a quantidade de dias do mês anterior ao mês da data atual - 86400000 Milisegundos = 1 dia
-
 while (mes < 0 || dia < 0) {
 // Ao subtrair 1 do mês ou do dia, os mesmos podem se tornar negativos, então o laço é repetido até que sejam positivos
 	if (mes < 0) {
@@ -24,6 +21,9 @@ while (mes < 0 || dia < 0) {
 	} // Se o mês for negativo, subtrai 1 do ano e adiciona 12 no mês
 
 	if (dia < 0) {
+		var diasDoMesAnt = new Date(Date.parse(new Date(dataAtual.getFullYear(), dataAtual.getMonth(), 1)) - 86400000).getDate();
+		// Retorna a quantidade de dias do mês anterior ao mês da data atual - 86400000 Milisegundos = 1 dia
+
 		mes--;
 		dia += diasDoMesAnt;
 	} // Se o dia for negativo, subtrai 1 do mês e adiciona os dias do mês anterior a data atual
